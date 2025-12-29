421cf28fe8aa410ade3bb198176ead3199717a52
kinzafatima_16 <kinzaqureshi1838@gmail.com>
2025-12-29 19:16:09 +0500

    Add files via upload

---
+++ python lists.py.py
@@ -0,0 +1,16 @@
+mylist=["apple","banana","cherry"]
+print (mylist[2])
+print (mylist[1:])
+print (mylist[:1])
+print (mylist[-3:-1])
+mylist[2]="mango"
+mylist.insert(2,"mango")
+print (mylist)
+mylist=[1,2,3,4,5,6,7,8]
+print (mylist[2:])
+mylist[2:6]=[4,5,6,7]
+print (mylist)
+(mylist[-4:-1])
+print (mylist)
+
+
