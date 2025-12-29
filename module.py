6b5a0492bf313a4b7dc39982e9c0af92538be1c1
kinzafatima_16 <kinzaqureshi1838@gmail.com>
2025-12-29 19:09:54 +0500

    Add files via upload

---
+++ module.py
@@ -0,0 +1,8 @@
+
+# Output -> Hello, Python!
+print('Hello, Python!')
+
+# Indentation or space before code line
+if 5 > 2:
+    print('Five is greater')
+
---
+++ strings2.py
@@ -0,0 +1,10 @@
+a = "my college"
+
+print(a.lower())
+print(a.upper())
+print(a.replace('m', 'z'))
+
+if "my" in a:
+    print("yes, 'my' in the string")
+elif "free" not in a:
+    print("no, 'free' not in the string")
