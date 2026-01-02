13b1c5c3a8734ac1859dc0cc53e2c584d4178cc7
kinzafatima_16 <kinzaqureshi1838@gmail.com>
2026-01-02 14:54:12 +0500

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+student="Ali"
+x=["bio","phy","chem","maths"]
+y=["100","99","98","97"]
+plt.bar("sub","marks")
+plt.xlabel("sub"),
+plt.ylabel("marks"),
+plt.title("result"),
+plt.show()
+
